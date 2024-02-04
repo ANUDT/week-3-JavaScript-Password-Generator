9c111571d5a7fdd19cf061c374c6afc6d471b593
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-02-04 18:15:38 +0000

    adding function, if statement and console log

--- script.js
+++ script.js
@@ -24,13 +24,14 @@ var numbers = "0123456789"
 var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 var lowercasePref = confirm ("Do you want lowercase in your password")
-var lowercasePref = confirm ("Do you want uppercase in your password")
-var lowercasePref = confirm ("Do you want namubers in your password")
-var lowercasePref = confirm ("Do you want special characters in your password")
+var uppercasecasePref = confirm ("Do you want uppercase in your password")
+var numbersPref = confirm ("Do you want numbers in your password")
+var specialPref = confirm ("Do you want special characters in your password")
 
 alert ("you must selct at least one of the type for your password")
 
 var passwordLenght = prompt ("How long do you want the password to be?")
+
 
 function passwordLenght()
 }
@@ -41,12 +42,30 @@ if (number in passwordLenght = > 120) {
   then (alert) ="password lenght is too long";
     }
  
-    var passwordCharacters = "";
+ 
+ var passwordCharacters = "";
 
-    if (lowercasePref) {
-      passwordCharacters += lowercase;
+if (lowercasePref) {
+  passwordCharacters += lowercase;
+}
 
+if (uppercasePref) {
+  passwordCharacters += uppercase;
+}
 
+if (numbersPref) {
+  passwordCharacters += numbers;
+}
+
+if (specialPref) {
+  passwordCharacters += special;
+}
+
+for (var i = 0; i <passwordLenght; i++) {
+  password += passwordCharacters.charAt(Math.floor(Math.random()* passwordCharacters
+}
+
+console.log(password)
 
 
 
