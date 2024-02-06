b4f6103d7b27e0e574362ea63eb5a53a11ba3cb5
ADT1 <152813453+ANUDT@users.noreply.github.com>
2024-02-06 19:12:31 +0000

    generated random word string in js

--- script.js
+++ script.js
@@ -54,9 +54,11 @@ if (passwordCharacters === "")
     return ("")
   }
 
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
+  var password = "";
+
+for (var i = 0; i < passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * passwordCharacters.length);
+  password += passwordCharacters.substring(randomNumber, randomNumber +1);
  }
 
  document.getElementById("password").value = password;
